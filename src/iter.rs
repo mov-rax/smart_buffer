2465421dcd416e7a0f117d15cfd9f0662e478ce6
mov-rax <csharper.work@gmail.com>
2021-01-03 01:09:42 -0600

    Added the start towards having SmartBuffers that have the Clone trait but not the Copy trait.

---
+++ src/iter.rs
@@ -0,0 +1,155 @@
+use crate::SmartBuffer;
+
+use core::mem::size_of;
+use alloc::alloc::{dealloc};
+
+impl<T, const N: usize> IntoIterator for SmartBuffer<T,N>
+    where T: Clone
+{
+    type Item = T;
+    type IntoIter = SmartBufferIter<T,N>;
+    /// Creates a consuming Iterator
+    fn into_iter(mut self) -> Self::IntoIter {
+        let stack_ptr = self.s_buf.as_mut_ptr();
+        let heap_ptr = self.d_buf;
+        let total_elem = self.size;
+
+        Self::IntoIter {
+            smart_buffer: self, // Self will be dropped when IntoIter is over
+            stack_ptr,
+            heap_ptr,
+            total_elem,
+            count: 0
+        }
+    }
+}
+
+impl<'a, T, const N: usize> IntoIterator for &'a SmartBuffer<T,N>
+    where T: Clone
+{
+    type Item = &'a T;
+    type IntoIter = SmartBufferIterRef<'a,T,N>;
+    /// Creates a consuming Iterator
+    fn into_iter(self) -> Self::IntoIter {
+        let stack_ptr = self.s_buf.as_ptr();
+        let heap_ptr = self.d_buf;
+        let total_elem = self.size;
+
+        Self::IntoIter {
+            smart_buffer: self,
+            stack_ptr,
+            heap_ptr,
+            total_elem,
+            count: 0
+        }
+
+    }
+}
+
+impl<'a, T, const N: usize> IntoIterator for &'a mut SmartBuffer<T,N>
+    where T: Clone
+{
+    type Item = &'a mut T;
+    type IntoIter = SmartBufferIterRefMut<'a,T,N>;
+    /// Creates a consuming Iterator
+    fn into_iter(self) -> Self::IntoIter {
+        let stack_ptr = self.s_buf.as_mut_ptr();
+        let heap_ptr = self.d_buf.clone();
+        let total_elem = self.size;
+
+        Self::IntoIter {
+            smart_buffer: self,
+            stack_ptr,
+            heap_ptr,
+            total_elem,
+            count: 0
+        }
+    }
+}
+
+/// Iterator for SmartBuffer where the SmartBuffer is Consumed
+pub struct SmartBufferIter<T, const N:usize>
+    where T: Clone
+{
+    smart_buffer: SmartBuffer<T,N>,
+    stack_ptr: *mut T,
+    heap_ptr: Option<*mut T>,
+    total_elem: usize,
+    count: usize,
+}
+
+impl<T, const N: usize> Iterator for SmartBufferIter<T,N>
+    where T: Clone
+{
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < self.total_elem{
+            if self.count < N{
+                self.count += 1;
+                return unsafe {Some((*((self.stack_ptr as usize + self.count - 1 * size_of::<T>()) as *mut T)).clone())}
+            }
+            self.count += 1;
+            return unsafe {Some((*((self.heap_ptr.unwrap() as usize + self.count - N - 1 * size_of::<T>()) as *mut T)).clone())}
+        }
+        None
+    }
+}
+
+/// Iterator for SmartBuffer where the SmartBuffer is immutably referenced to
+pub struct SmartBufferIterRef<'a, T, const N:usize>
+    where T: 'a + Clone
+{
+    smart_buffer: &'a SmartBuffer<T,N>,
+    stack_ptr: *const T,
+    heap_ptr: Option<*mut T>, // will not change values in heap_ptr
+total_elem: usize,
+    count: usize,
+}
+
+impl<'a, T, const N: usize> Iterator for SmartBufferIterRef<'a, T,N>
+    where T: 'a + Clone
+{
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < self.total_elem{
+            if self.count < N{
+                self.count += 1;
+                return unsafe {Some(&*((self.stack_ptr as usize + self.count - 1 * size_of::<T>()) as *const T))}
+            }
+            self.count += 1;
+            return unsafe {Some(&*((self.heap_ptr.unwrap() as usize + self.count - N - 1 * size_of::<T>()) as *const T))}
+        }
+        None
+    }
+}
+
+/// Iterator for SmartBuffer where SmartBuffer is mutably referenced to
+pub struct SmartBufferIterRefMut<'a, T, const N:usize>
+    where T: 'a + Clone
+{
+    smart_buffer: &'a mut SmartBuffer<T,N>,
+    stack_ptr: *mut T,
+    heap_ptr: Option<*mut T>, // will not change values in heap_ptr
+total_elem: usize,
+    count: usize,
+}
+
+impl<'a, T, const N: usize> Iterator for SmartBufferIterRefMut<'a, T,N>
+    where T: 'a + Clone
+{
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < self.total_elem{
+            if self.count < N{
+                self.count += 1;
+                return unsafe {Some(&mut *((self.stack_ptr as usize + self.count - 1 * size_of::<T>()) as *mut T))}
+            }
+            self.count += 1;
+            return unsafe {Some(&mut *((self.heap_ptr.unwrap() as usize + self.count - N - 1 * size_of::<T>()) as *mut T))}
+        }
+        None
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,22 @@
 extern crate alloc;
 use alloc::alloc::{alloc, dealloc, Layout};
 use core::mem::size_of;
+
+mod iter;
+
 #[cfg(test)]
 mod tests {
+    use crate::SmartBuffer;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let buf = SmartBuffer::<&str, 10>::new("",25);
     }
 }
 
 #[feature(min_const_generics)]
 pub struct SmartBuffer<T, const N:usize>
-    where T: Copy + Clone
+    where T: Clone
 {
     s_buf: [T; N],
     d_buf: Option<*mut T>,
@@ -25,7 +30,7 @@ cursor: usize,
 }
 
 impl<T, const N:usize> SmartBuffer<T,N>
-    where T: Copy + Clone
+    where T: Clone
 {
 
     /// Safely push a value into the SmartBuffer
@@ -49,14 +54,14 @@ impl<T, const N:usize> SmartBuffer<T,N>
     /// Safely inserts a slice of data, starting at the size.
     pub fn insert_slice(&mut self, slice: &[T]){
         for elem in slice{
-            self.push(*elem);
+            self.push(elem.clone());
         }
     }
 
     /// Safely inserts a slice of data at an index;
     pub fn insert_slice_at(&mut self, slice: &[T], mut index:usize){
         for elem in slice{
-            self.insert(*elem, index);
+            self.insert(elem.clone(), index);
             index += 1;
         }
     }
@@ -65,7 +70,7 @@ impl<T, const N:usize> SmartBuffer<T,N>
     /// Safely inserts an array, starting at the size.
     pub fn insert_arr<const M: usize>(&mut self, arr: &[T; M]){
         for elem in arr{
-            self.push(*elem);
+            self.push(elem.clone());
         }
     }
 
@@ -85,21 +90,21 @@ impl<T, const N:usize> SmartBuffer<T,N>
     }
 
     /// Safely get a value at an index
-    pub fn get(&mut self, index:usize) -> Option<T> {
+    pub fn get(&mut self, index:usize) -> Option<&T> {
         if index < N {
-            return Some(self.s_buf[index])
+            return Some(&self.s_buf[index])
         } else if index < self.capacity {
-            return unsafe { Some(*((self.d_buf.unwrap() as usize + (index - N) * size_of::<T>()) as *mut T)) }
+            return unsafe { Some(&*((self.d_buf.unwrap() as usize + (index - N) * size_of::<T>()) as *mut T)) }
         }
         None
     }
 
     /// Unsafely get a value at an index. An index too large will result in a fault
-    pub unsafe fn get_unchecked(&mut self, index:usize) -> T{
+    pub unsafe fn get_unchecked(&mut self, index:usize) -> &T{
         if index < N{
-            return self.s_buf[index];
+            return &self.s_buf[index];
         }
-        *((self.d_buf.unwrap() as usize + (index - N) * size_of::<T>()) as *mut T)
+        &*((self.d_buf.unwrap() as usize + (index - N) * size_of::<T>()) as *mut T)
     }
 
     pub fn as_mut_ptr(mut self) -> *mut Self{
@@ -108,7 +113,7 @@ impl<T, const N:usize> SmartBuffer<T,N>
 
     /// Safely allocate extra heap memory
     fn allocate(&mut self, elements:usize){
-        let layout = Layout::from_size_align(elements*size_of::<T>(), size_of::<T>()); // aligned to itself
+        let layout = Layout::from_size_align(elements*size_of::<T>(), 1);
         if let Ok(layout) = layout{
             let ptr = unsafe {alloc(layout) as *mut T};
             self.capacity += layout.size();
@@ -116,8 +121,10 @@ impl<T, const N:usize> SmartBuffer<T,N>
             self.d_buf = Some(ptr);
         }
     }
-
-    pub fn new(value: T, len:usize) -> Self{
+    // creates a SmartBuffer where the default value is set to the value entered
+    pub fn new(value: T, len:usize) -> Self
+    where T: Copy + Clone
+    {
         let mut buf = Self{
             s_buf: [value; N],
             d_buf: None,
@@ -133,14 +140,21 @@ impl<T, const N:usize> SmartBuffer<T,N>
         buf
     }
 
+    // /// Creates a SmartBuffer where the default value is the first value entered.
+    // ///
+    // /// This function is not recommended
+    // pub fn gen(buf:[T; N], len:usize) -> Self{
+    //
+    // }
+
 }
 
 impl<T, const N:usize> SmartBuffer<T,N>
-    where T: Copy + Clone + PartialEq
+    where T:Clone + PartialEq
 {
     /// Recalculates the size
     pub fn calc_size(&mut self){
-        let default = self.default;
+        let default = self.default.clone();
 
         let mut size = 0;
         for elem in &*self{
@@ -153,160 +167,8 @@ impl<T, const N:usize> SmartBuffer<T,N>
     }
 }
 
-
-impl<T, const N: usize> IntoIterator for SmartBuffer<T,N>
-    where T: Copy + Clone
-{
-    type Item = T;
-    type IntoIter = SmartBufferIter<T,N>;
-    /// Creates a consuming Iterator
-    fn into_iter(mut self) -> Self::IntoIter {
-        let stack_ptr = self.s_buf.as_mut_ptr();
-        let heap_ptr = self.d_buf;
-        let total_elem = self.size;
-
-        Self::IntoIter {
-            smart_buffer: self, // Self will be dropped when IntoIter is over
-            stack_ptr,
-            heap_ptr,
-            total_elem,
-            count: 0
-        }
-    }
-}
-
-impl<'a, T, const N: usize> IntoIterator for &'a SmartBuffer<T,N>
-    where T: Copy + Clone
-{
-    type Item = &'a T;
-    type IntoIter = SmartBufferIterRef<'a,T,N>;
-    /// Creates a consuming Iterator
-    fn into_iter(self) -> Self::IntoIter {
-        let stack_ptr = self.s_buf.as_ptr();
-        let heap_ptr = self.d_buf;
-        let total_elem = self.size;
-
-        Self::IntoIter {
-            smart_buffer: self,
-            stack_ptr,
-            heap_ptr,
-            total_elem,
-            count: 0
-        }
-
-    }
-}
-
-impl<'a, T, const N: usize> IntoIterator for &'a mut SmartBuffer<T,N>
-    where T: Copy + Clone
-{
-    type Item = &'a mut T;
-    type IntoIter = SmartBufferIterRefMut<'a,T,N>;
-    /// Creates a consuming Iterator
-    fn into_iter(self) -> Self::IntoIter {
-        let stack_ptr = self.s_buf.as_mut_ptr();
-        let heap_ptr = self.d_buf;
-        let total_elem = self.size;
-
-        Self::IntoIter {
-            smart_buffer: self,
-            stack_ptr,
-            heap_ptr,
-            total_elem,
-            count: 0
-        }
-    }
-}
-
-/// Iterator for SmartBuffer where the SmartBuffer is Consumed
-pub struct SmartBufferIter<T, const N:usize>
-    where T: Copy + Clone
-{
-    smart_buffer: SmartBuffer<T,N>,
-    stack_ptr: *mut T,
-    heap_ptr: Option<*mut T>,
-    total_elem: usize,
-    count: usize,
-}
-
-impl<T, const N: usize> Iterator for SmartBufferIter<T,N>
-    where T: Copy + Clone
-{
-    type Item = T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.count < self.total_elem{
-            if self.count < N{
-                self.count += 1;
-                return unsafe {Some(*((self.stack_ptr as usize + self.count - 1 * size_of::<T>()) as *mut T))}
-            }
-            self.count += 1;
-            return unsafe {Some(*((self.heap_ptr.unwrap() as usize + self.count - N - 1 * size_of::<T>()) as *mut T))}
-        }
-        None
-    }
-}
-
-/// Iterator for SmartBuffer where the SmartBuffer is immutably referenced to
-pub struct SmartBufferIterRef<'a, T, const N:usize>
-    where T: 'a + Copy + Clone
-{
-    smart_buffer: &'a SmartBuffer<T,N>,
-    stack_ptr: *const T,
-    heap_ptr: Option<*mut T>, // will not change values in heap_ptr
-total_elem: usize,
-    count: usize,
-}
-
-impl<'a, T, const N: usize> Iterator for SmartBufferIterRef<'a, T,N>
-    where T: 'a + Copy + Clone
-{
-    type Item = &'a T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.count < self.total_elem{
-            if self.count < N{
-                self.count += 1;
-                return unsafe {Some(&*((self.stack_ptr as usize + self.count - 1 * size_of::<T>()) as *const T))}
-            }
-            self.count += 1;
-            return unsafe {Some(&*((self.heap_ptr.unwrap() as usize + self.count - N - 1 * size_of::<T>()) as *const T))}
-        }
-        None
-    }
-}
-
-/// Iterator for SmartBuffer where SmartBuffer is mutably referenced to
-pub struct SmartBufferIterRefMut<'a, T, const N:usize>
-    where T: 'a + Copy + Clone
-{
-    smart_buffer: &'a mut SmartBuffer<T,N>,
-    stack_ptr: *mut T,
-    heap_ptr: Option<*mut T>, // will not change values in heap_ptr
-total_elem: usize,
-    count: usize,
-}
-
-impl<'a, T, const N: usize> Iterator for SmartBufferIterRefMut<'a, T,N>
-    where T: 'a + Copy + Clone
-{
-    type Item = &'a mut T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.count < self.total_elem{
-            if self.count < N{
-                self.count += 1;
-                return unsafe {Some(&mut *((self.stack_ptr as usize + self.count - 1 * size_of::<T>()) as *mut T))}
-            }
-            self.count += 1;
-            return unsafe {Some(&mut *((self.heap_ptr.unwrap() as usize + self.count - N - 1 * size_of::<T>()) as *mut T))}
-        }
-        None
-    }
-}
-
 impl<T, const N:usize> Drop for SmartBuffer<T,N>
-    where T: Copy + Clone
+    where T: Clone
 {
     fn drop(&mut self) {
         if let Some(ptr) = self.d_buf{
@@ -314,4 +176,6 @@ impl<T, const N:usize> Drop for SmartBuffer<T,N>
         }
     }
 }
+
+macro_rules!
 
